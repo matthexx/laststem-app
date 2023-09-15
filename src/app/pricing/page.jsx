35d4e9dca77e0733d1d3003453b76e5241c31ae9
Martins Daniels <danielsmrtns@gmail.com>
2023-09-15 07:29:07 -0400

    added an empty  pricing page so that the route works in the home page, instead of returning a 404 error

---
+++ src/app/pricing/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./page.css";
+
+
+const Pricing = () => {
+  return (
+    <div className="pricing">
+      <div className="pricing-hero">This is the Pricing page</div>
+    </div>
+  );
+};
+
+export default Pricing;
