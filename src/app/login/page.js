91cee71e8bf88ef859e6f5228183db502b38883c
Martins Daniels <danielsmrtns@gmail.com>
2023-09-15 07:26:13 -0400

    added padding to the login page form

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -69,7 +69,7 @@ export default function LoginPage(){
                         onChange={(e)=>setUser({
                             ...user, email: e.target.value
                         })}
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                     </div>
                 </div>
@@ -96,7 +96,7 @@ export default function LoginPage(){
                         onChange={(e)=>setUser({
                             ...user, password: e.target.value
                         })}
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                     </div>
                 </div>
